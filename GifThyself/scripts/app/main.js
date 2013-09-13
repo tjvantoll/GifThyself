adfd0a23d17bf07fd2bc7084f7bd17b4d55d554f
TJ VanToll <TJ.VanToll@telerik.com>
2013-09-13 09:35:12 -0500

    Adding comment to test GitHub integration again.

--- GifThyself/scripts/app/main.js
+++ GifThyself/scripts/app/main.js
@@ -108,6 +108,7 @@
             var binaryGif = encoder.stream().getData(),
                 dataUrl = 'data:image/gif;base64,' + encode64(binaryGif);
             
+            // Upload the gif to Everlive
             uploadGif(binaryGif);
 
             $('<img>').attr('src', dataUrl).load(function() {
