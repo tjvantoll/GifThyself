20fd95e6417454d32843a561ad5a2ae5abe62461
TJ VanToll <TJ.VanToll@telerik.com>
2013-09-17 07:53:50 -0500

    Select text in URL <input> onfocus.

--- GifThyself/scripts/app/main.js
+++ GifThyself/scripts/app/main.js
@@ -177,6 +177,13 @@
     	    .on('click', '#images button', removeImage)
         	.on('click', '.confirmation-cancel', closeConfirmationDialog)
         	.on('click', '#build-another', resetApp)
+	        .on('focus', 'input', function() {
+                // See http://stackoverflow.com/questions/3272089/programmatically-selecting-text-in-an-input-field-on-ios-devices-mobile-safari#answer-7436574
+                var self = this;
+                setTimeout(function() {
+                    self.setSelectionRange(0, 9999);
+                });
+            })
             .on('click', '#viewOnline', function(event) {
             	event.preventDefault();
                 window.open($(this).attr('href'), '_blank');
