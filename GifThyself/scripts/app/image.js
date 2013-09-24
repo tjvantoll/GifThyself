31e8b144790773939882ba23d328833367a4438f
Burke Holland <burkeholland@gmail.com>
2013-09-24 18:17:06 -0500

    Add share model. Break out init code into global functions.

--- GifThyself/scripts/app/image.js
+++ GifThyself/scripts/app/image.js
@@ -1,7 +1,7 @@
 (function() {
     
     // Holder for the gif after it has been created but before it's uploaded.
-    var base64EncodedGif;
+    var base64EncodedGif, url;
     
     function generateQuickGuid() {
         return Math.random().toString(36).substring(2, 15) +
@@ -100,52 +100,75 @@
         $('#shareGif').prop('disabled', true);
     };
     
-    window.imageInit = function() {
-        $('#buildGif').data('kendoMobileButton').bind('click', function() {
-            if (checkForImages()) {
-                app.navigate('#preview');
-                $(app.pane.loader.element).find('h1').text('Building...');
-                $('#preview-container').addClass('loading');
-                app.showLoading();
-                setTimeout(buildGif, 3000);   
-            }
-        });
-        
-        $('#shareGif').data('kendoMobileButton').bind('click', function() {
-            closeConfirmationDialog();
-            app.navigate('#share');
-            $(app.pane.loader.element).find('h1').text('Uploading...');
-            var container = $('#share-results-container').addClass('loading');
-            app.showLoading();
-            setTimeout(function() {
-                uploadGif().then(function(data) {
-                    var url = data.result.Uri;
-                    container.find('input').val(url);
-                    container.find('p a').attr('href', url);
-                    app.hideLoading();
-                    container.removeClass('loading');
-                }); 
-            }, 3000);
-        });
-        
-        $('.confirmation-cancel').each(function() {
-			$(this).data('kendoMobileButton').bind('click', closeConfirmationDialog); 
-        });
-        
-        $('#getPicture').data('kendoMobileButton').bind('click', getPicture);
-        
-        $(document)
-            .on('click', '#images button', removeImage)
-            .on('focus', 'input', function() {
-                // See http://stackoverflow.com/questions/3272089/programmatically-selecting-text-in-an-input-field-on-ios-devices-mobile-safari#answer-7436574
-                var self = this;
-                setTimeout(function() {
-                    self.setSelectionRange(0, 9999);
-                });
-            })
-            .on('click', '#viewOnline', function(event) {
-                event.preventDefault();
-                window.open($(this).attr('href'), '_blank');
+    function share() {
+        window.plugins.social.share("#gifthyself", url);
+    }
+    
+    window.init = {
+        build: function() {
+            $('#buildGif').data('kendoMobileButton').bind('click', function() {
+                if (checkForImages()) {
+                    app.navigate('#preview');
+                    $(app.pane.loader.element).find('h1').text('Building...');
+                    $('#preview-container').addClass('loading');
+                    app.showLoading();
+                    setTimeout(buildGif, 3000);   
+                }
             });
-    };
+            
+            $('#getPicture').data('kendoMobileButton').bind('click', getPicture);
+        },
+        confirm: function() {
+        	$('#shareGif').data('kendoMobileButton').bind('click', function() {
+                app.navigate('#share');
+                $(app.pane.loader.element).find('h1').text('Uploading...');
+                var container = $('#share-results-container').addClass('loading');
+                app.showLoading();
+                setTimeout(function() {
+                    uploadGif().then(function(data) {
+                        window.models.share.set("url", data.result.Uri);
+                        app.hideLoading();
+                        container.removeClass('loading');
+                    }); 
+                }, 3000);
+        	});    
+        }    
+    }
+        
+    $(document)
+        .on('click', '#images button', removeImage)
+        .on('focus', 'input', function() {
+            // See http://stackoverflow.com/questions/3272089/programmatically-selecting-text-in-an-input-field-on-ios-devices-mobile-safari#answer-7436574
+            var self = this;
+            setTimeout(function() {
+                self.setSelectionRange(0, 9999);
+            });
+        })
+        .on('click', '#viewOnline', function(event) {
+            event.preventDefault();
+            window.open($(this).attr('href'), '_blank');
+        });
+    
+    window.models = {};
+    window.models.share = (function() {
+        var viewModel = kendo.observable({
+            url: "",
+            events: {
+                share: function(e) {
+                    window.plugins.social.share("#gifthyself", null, viewModel.get("url"));
+                },
+                view: function(e) {
+                    window.open(viewModel.get("url"), '_blank');
+                }
+            },
+            support: {
+                sharing: function() {
+                    return window.app.os.ios;
+                }
+            }
+		});
+                    
+        return viewModel;
+    }());
+            
 }());
